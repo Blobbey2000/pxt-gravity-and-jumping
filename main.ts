1c8fe574d982892d8dca03365583431c19501715
Blobbey2000 <116673732+Blobbey2000@users.noreply.github.com>
2023-04-12 19:43:30 -0700

    More straightforward blocks

--- main.ts
+++ main.ts
@@ -1,1 +1,28 @@
- 
+//% blockGap="8"
+namespace sprites {
+
+    /**
+    * Sets sprite gravity.
+    */
+    //% block="set $sprite=variables_get gravity to $gravity"
+    //% group="Physics"
+    //% sprite.shadow=variables_get
+    //% sprite.defl=mySprite
+    export function setSpriteGravityTo(sprite: Sprite, gravity: number) {
+        sprite.ay = gravity
+    }
+
+    /**
+    * Makes sprite jump.
+    */
+    //% block="make $sprite=variables_get jump $jump"
+    //% group="Physics"
+    //% sprite.shadow=variables_get
+    //% sprite.defl=mySprite
+    export function makeSpriteJump(sprite: Sprite, jump: number) {
+        sprite.vy = jump * -1
+    }
+
+}
+
+
